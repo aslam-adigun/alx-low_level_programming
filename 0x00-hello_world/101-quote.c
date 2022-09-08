3d6e4644ab68d09a680f798953ce221376214f5e
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-08 09:29:44 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
-#include<stdio.h>
-#include<string.h>
-#include<unistd.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -9,6 +7,7 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, str, 59);
 	return (1);
 }

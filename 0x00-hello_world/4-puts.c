fdb21a1caf08818a9ab70b69546d6882400fee4f
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-08 08:15:58 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: String
+ */
+int main(void)
+{
+	char Mystr[} = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}

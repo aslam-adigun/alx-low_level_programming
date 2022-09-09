7c3780aafb39e7259607325d1810ecdd66fd1993
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-09 04:50:21 -0700

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print numbers 0 to 9'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+		if (m != 57)
+		{
+			putchar(', ');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

d06c8c21115f4befca9cdca4fe6a729ff6f10d59
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-09 04:00:24 -0700

    Print tebahpla in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print letters of the alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int m;
+
+	for (m = 0; m < 26; m++)
+	{
+		putchar(alp[m]);
+	}
+
+	putchar('\n');
+	return (0);
+}

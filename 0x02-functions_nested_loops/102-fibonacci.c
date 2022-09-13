7b11d64d175b847d569ca5ba98d9a57e752a7783
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-13 12:56:52 -0700

    Print first 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,14 +12,15 @@ int main(void)
 	int n = 50, f1 = 0, f2 = 1, i;
 
 	if (n < 1)
-		return;
-	printf("%d ", f1);
-	for (i = 1; i < n; i++)
 	{
-		printf("%d ", f2);
-		int next = f1 + f2;
+		printf("%d ", f1);
+		for (i = 1; i < n; i++)
+		{
+			printf("%d ", f2);
+			int next = f1 + f2;
 
-		f1 = f2;
-		f2 = next;
+			f1 = f2;
+			f2 = next;
+		}
 	}
 }

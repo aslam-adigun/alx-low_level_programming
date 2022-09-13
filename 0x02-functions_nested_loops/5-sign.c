fd449abcde19a7c7e40345dd5ad326987da406aa
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-13 08:46:58 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - show if a number is less, greater or = 0
+ *
+ * @n: variable to be checked
+ *
+ * Return: Always 0 (success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+, ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0, ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-, ');
+		return (0);
+	}
+	_putchar('\n');
+}

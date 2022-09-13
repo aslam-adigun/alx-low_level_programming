63ab4619b3f2acd007db313f3ea7aacf7333e48b
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-13 08:10:49 -0700

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Print alphabets ten times and add a new line
+ *
+ * @c: variable to be checked
+ *
+ * Return: Always 0 (success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		_putchar(1)
+	}
+	else
+		_putchar(0)
+}

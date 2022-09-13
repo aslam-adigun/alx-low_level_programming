11496628e79728f4aedd8ac40ab3901246180dca
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-13 07:31:44 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabets and add a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main()
+{
+	void print_alphabet(void)
+	{
+		int m;
+
+		for (m = 97; m <= 122; m++)
+
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ *
+ * _putchar - writes the character c to stdout
+ *
+ * @p: The character to print
+ *
+ *
+ * Return: On success 1.
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+
+int _putchar(char p)
+
+{
+
+	return (write(1, &p, 1));
+
+}

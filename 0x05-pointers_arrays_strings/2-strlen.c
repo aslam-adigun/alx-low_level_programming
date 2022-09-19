57a15dc9bedc3edd34f4ab33daa15181e5aa2343
Aslam Dhikrullahi <a.adepoju23@gmail.com>
2022-09-19 09:18:05 -0700

    StrLen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return the lenght of a string
+ * @s: string pointer
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	
+	while (*(s + count) != '\0')
+	count++;
+	return (count);
+}
